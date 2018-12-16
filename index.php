8eb7a35ec4cd4a685ab3a1a32d64a03de065c8e5
ksenia <raddabuy@yandex.ru>
2018-12-16 22:34:36 +0300

    вывод статей на страницу

--- index.php
+++ index.php
@@ -11,12 +11,28 @@
     <main class="container mt-5">
       <div class="row">
           <div class="col-md-8 mb-5">
-              Основная часть сайта
+              <?php
+              require_once 'mysql_connect.php';
+
+              $sql = 'SELECT * FROM `articles` ORDER BY `date` DESC';
+              $query = $pdo->query($sql);
+
+              while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+                echo "<h2>$row->title</h2>
+                <p>$row->intro</p>
+                <p><b>Автор статьи:</b> <mark>$row->author</mark></p>
+                <a href='news.php?id=$row->id' title='$row->title'>
+                  <button class='btn btn-warning mb-50'>Прочитать больше</button>
+                </a>";
+              }
+
+               ?>
           </div>
           <?php require 'blocks/aside.php'; ?>
       </div>
     </main>
     <?php require 'blocks/footer.php'; ?>
+
 
 </body>
 </html>
